8b80809202ba744802737b7229f48c0c05539b7f
Amira <amira@omniphics.com>
2015-12-21 11:07:04 +0800

    Initial

---
+++ index.php
@@ -0,0 +1,56 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>2048</title>
+	<style type="text/css">
+	body{
+
+	}
+	 td {
+	 	border: 1px solid black;
+	 	height: 40px;
+	 	width: 40px;
+	 }
+
+	 .tile{
+	 	background-color: black;
+	 	color:white; 
+	 }
+
+	 .tile.numbered{
+	 	background-color: blue;
+	 }
+	</style>
+</head>
+<body>
+<table>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+</table>
+<script type="text/javascript">
+	
+</script>
+</body>
+</html>
