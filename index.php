f7060beb714f69640e09325aa6013fd9ae3e2569
Amira <amira@omniphics.com>
2015-12-31 15:06:44 +0800

    Version 1 DONE
    - Can play the game properly
    - There’s a bug with the order of merge for left or right, i.e, if
    there’s 3 same squares in a row, and you click ‘right’, square 1 and 2
    will merge, instead of square 2 and 3.

--- index.php
+++ index.php
@@ -26,6 +26,19 @@
 	 	display: table-cell;
 	    vertical-align: middle;
 	 }
+
+	 .tile[data-value="4"]{background-color: red;}
+	 .tile[data-value="8"]{background-color: orange;}
+	 .tile[data-value="16"]{background-color: mistyrose;}
+	 .tile[data-value="32"]{background-color: green;}
+	 .tile[data-value="64"]{background-color: azure;}
+	 .tile[data-value="128"]{background-color: violet;}
+	 .tile[data-value="256"]{background-color: aquamarine;}
+	 .tile[data-value="512"]{background-color: firebrick;}
+	 .tile[data-value="1024"]{background-color: fuchsia;}
+	 .tile[data-value="2048"]{background-color: thistle;}
+	 .tile[data-value="4096"]{background-color: teal;}
+
 	</style>
 </head>
 <body>
@@ -94,18 +107,12 @@ $(document).ready(function(){
 		
 		var loop_move = false; //flag to check if there was a merge, if true, do another move loop
 		
-		if(direction == 'up'){
+		if(direction == 'up' || direction == 'left'){
 			$tiles = $('.tile');
 		}
-		else if(direction == 'down'){
+		else if(direction == 'down' || direction == 'right'){
 			$tiles = $('.tile').reverse();
 			
-		}
-		else if(direction == 'left'){
-			
-		}
-		else if(direction == 'right'){
-
 		}
 
 		$tiles.each(function(){
@@ -114,26 +121,33 @@ $(document).ready(function(){
 			var x = parseInt($(this).attr('data-x'));
 			var y = parseInt($(this).attr('data-y'));
 			var tile_id = $(this).attr('id');
-			var target_sqr = 0;
+			// var target_sqr = 0;
 
 			if(direction == 'up'){
 				if(y>1){
 					movable = true;
-					target_sqr = y-1;
+					// target_sqr = y-1;
 				}
 			}
 			else if(direction == 'down'){
 				if(y<4){
 					movable = true;
-					target_sqr = y+1;
+					// target_sqr = y+1;
 				}
 				
 			}
 			else if(direction == 'left'){
+				if(x>1){
+					movable = true;
+					// target_sqr = x-1;
+				}
 				
 			}
 			else if(direction == 'right'){
-
+				if(x<4){
+					movable = true;
+					// target_sqr = x+1;
+				}
 			}
 
 			if(movable){
@@ -148,29 +162,34 @@ $(document).ready(function(){
 					x = parseInt($tile.attr('data-x'));
 					y = parseInt($tile.attr('data-y'));
 
-					if(direction == 'up'){
-						target_sqr = y-1;
-					}
-					else if(direction == 'down'){
-						target_sqr = y+1;
-						
-					}
-					else if(direction == 'left'){
-						
-					}
-					else if(direction == 'right'){
-
-					}
-					
 					console.log("MOVE "+direction+": ",
-					"current position=> ", "("+x+","+y+")",
-					"target position=> ", "("+x+","+target_sqr+")");
+					"current position=> ", "("+x+","+y+")");
 
 					$current_sqr = $('.square[data-row='+ y +'][data-col='+x+']');
-					$target_sqr = $('.square[data-row='+ target_sqr +'][data-col='+x+']');
+
+					if(direction == 'up'){
+						y = y-1;
+					}
+					else if(direction == 'down'){
+						y = y+1;
+					}
+					else if(direction == 'left'){
+						x = x-1;
+					}
+					else if(direction == 'right'){
+						x = x+1;
+					}
+					
+					console.log("target position=> ", "("+x+","+y+")");
+
+					// $current_sqr = $('.square[data-row='+ y +'][data-col='+x+']');
+					$target_sqr = $('.square[data-row='+ y +'][data-col='+x+']');
 
 					$target_sqr.html($('#'+tile_id));
-					$tile.attr('data-y', target_sqr);
+
+					$tile.attr('data-x', x);
+					$tile.attr('data-y', y);
+
 					$current_sqr.html("");
 					
 					myGame.toggle_tiled_squares();
@@ -189,35 +208,46 @@ $(document).ready(function(){
 	}
 
 	Game.prototype.should_i_move = function(tile_id, direction, check_only){ 
-		var should_i_move = true, x, y, target_sqr;
+		var should_i_move = true, x, y;
 		x = parseInt($('#'+tile_id).attr('data-x'));
 		y = parseInt($('#'+tile_id).attr('data-y'));
+		
+		if(!check_only){
+			console.log("inside move check: ",
+						"current position=> ", "("+x+","+y+")");
+		}
 
 		if(direction == 'up'){
-			target_sqr = y-1;
+			y = y-1;
 		}
 		else if(direction == 'down'){
-			target_sqr = y+1;
+			y = y+1;
 			
 		}
 		else if(direction == 'left'){
+			x = x-1;
 			
 		}
 		else if(direction == 'right'){
+			x = x+1;
 
 		}
 
 		if(!check_only){
-			console.log(
-				'target_sqr=> ', target_sqr, 
-				'x=> ', x, 
-				'y=> ', y, 
-				'target_sqr_val=> ',$('.square[data-row='+target_sqr+'][data-col='+ x +'] .tile').html(), 
-				'tile_val=> ', $('#'+tile_id).html()
-			);
+			// console.log(
+			// 	'target_sqr=> ', target_sqr, 
+			// 	'x=> ', x, 
+			// 	'y=> ', y, 
+			// 	'target_sqr_val=> ',$('.square[data-row='+target_sqr+'][data-col='+ x +'] .tile').html(), 
+			// 	'tile_val=> ', $('#'+tile_id).html()
+			// );
+
+			console.log("target position=> ", "("+x+","+y+")", 
+				'target_sqr_val=> ',$('.square[data-row='+y+'][data-col='+ x +'] .tile').html(), 
+				'tile_val=> ', $('#'+tile_id).html());
 		}
 
-		if(!$('.square[data-row='+target_sqr+'][data-col='+ x +']').hasClass('empty')){
+		if(!$('.square[data-row='+y+'][data-col='+ x +']').hasClass('empty')){
 			should_i_move = false;
 		}
 
@@ -230,18 +260,12 @@ $(document).ready(function(){
 		loop_merge = false, //flag to check if there was a merge, if true, do another move loop
 		tile_count = $('.tile').length; //count total tiles before entering loop to equate with loop counter
 
-		if(direction == 'up'){
+		if(direction == 'up' || direction == 'left'){
 			$tiles = $('.tile').reverse();
 		}
-		else if(direction == 'down'){
+		else if(direction == 'down' || direction == 'right'){
 			$tiles = $('.tile');
 			
-		}
-		else if(direction == 'left'){
-			
-		}
-		else if(direction == 'right'){
-
 		}
 
 		$tiles.each(function(){
@@ -250,25 +274,32 @@ $(document).ready(function(){
 			var x = parseInt($(this).attr('data-x'));
 			var y = parseInt($(this).attr('data-y'));
 			var tile_id = $(this).attr('id');
-			var target_sqr = 0;
 
 			if(direction == 'up'){
 				if(y>1){
 					mergable = true;
-					target_sqr = y-1;
+					// y = y-1;
 				}
 			}
 			else if(direction == 'down'){
 				if(y<4){
 					mergable = true;
-					target_sqr = y+1;
+					// y = y+1;
 				}
 				
 			}
 			else if(direction == 'left'){
+				if(x>1){
+					mergable = true;
+					// x = x-1;
+				}
 				
 			}
 			else if(direction == 'right'){
+				if(x<4){
+					mergable = true;
+					// x = x+1;
+				}
 
 			}
 
@@ -286,33 +317,36 @@ $(document).ready(function(){
 					$tile = $('#'+tile_id);
 					x = parseInt($tile.attr('data-x'));
 					y = parseInt($tile.attr('data-y'));
+					
+					$current_sqr = $('.square[data-row='+ y +'][data-col='+x+']');
 
 					if(direction == 'up'){
-						target_sqr = y-1;
+						y = y-1;
 					}
 					else if(direction == 'down'){
-						target_sqr = y+1;
+						y = y+1;
 						
 					}
 					else if(direction == 'left'){
+						x = x-1;
 						
 					}
 					else if(direction == 'right'){
+						x = x+1;
 
 					}
 
-					console.log("this is the tile=> ", $('#'+tile_id), "with x=> ", x, "with y=> ", y);
-
-					$current_sqr = $('.square[data-row='+ y +'][data-col='+x+']');
-					$target_sqr = $('.square[data-row='+ target_sqr +'][data-col='+x+']');
+					$target_sqr = $('.square[data-row='+ y +'][data-col='+x+']');
 
 					new_val = $tile.html()*2;
 
 					console.log("MERGE. new_val=> ",new_val, "for sqr=> ", $target_sqr);
 					$('#'+tile_id).html(new_val);
-					$('#'+tile_id).css('background-color', 'violet');
+					$('#'+tile_id).attr('data-value', new_val);
+					// $('#'+tile_id).css('background-color', 'violet');
 					$target_sqr.html($('#'+tile_id));
-					$tile.attr('data-y', target_sqr);
+					$tile.attr('data-x', x);
+					$tile.attr('data-y', y);
 					$tile.attr('data-merged', '1');
 					$current_sqr.html("");
 					
@@ -340,40 +374,51 @@ $(document).ready(function(){
 	}
 
 	Game.prototype.should_i_merge = function(tile_id, direction, check_only){ 
-		var should_i_merge = false, x, y, target_sqr;
+		var should_i_merge = false, x, y;
 		x = parseInt($('#'+tile_id).attr('data-x'));
 		y = parseInt($('#'+tile_id).attr('data-y'));
 		is_merged = $('#'+tile_id).attr('data-merged');
+		
+		if(!check_only){
+			console.log("inside merge check: ",
+						"current position=> ", "("+x+","+y+")");
+		}
 
 		if(direction == 'up'){
-			target_sqr = y-1;
+			y = y-1;
 		}
 		else if(direction == 'down'){
-			target_sqr = y+1;
+			y = y+1;
 			
 		}
 		else if(direction == 'left'){
+			x = x-1;
 			
 		}
 		else if(direction == 'right'){
+			x = x+1;
 
 		}
 
 		if(!check_only){
-			console.log("INSIDE should_i_merge",
-				'tile_id=> ', tile_id, 
-				'tile_id=> ', tile_id, 
-				'is_merged=> ', is_merged, 
-				'target_sqr=> ', target_sqr, 
-				'x=> ', x, 
-				'y=> ', y, 
-				'target_sqr_val=> ',$('.square[data-row='+target_sqr+'][data-col='+ x +'] .tile').html(), 
-				'tile_val=> ', $('#'+tile_id).html()
-			);
+			// console.log("INSIDE should_i_merge",
+			// 	'tile_id=> ', tile_id, 
+			// 	'tile_id=> ', tile_id, 
+			// 	'is_merged=> ', is_merged, 
+			// 	'target_sqr=> ', target_sqr, 
+			// 	'x=> ', x, 
+			// 	'y=> ', y, 
+			// 	'target_sqr_val=> ',$('.square[data-row='+target_sqr+'][data-col='+ x +'] .tile').html(), 
+			// 	'tile_val=> ', $('#'+tile_id).html()
+			// );
+
+			console.log("target position=> ", "("+x+","+y+")", 
+				'target_sqr_val=> ',$('.square[data-row='+y+'][data-col='+ x +'] .tile').html(), 
+				'tile_val=> ', $('#'+tile_id).html());
 		}
 		
 		if($('#'+tile_id).length && is_merged == '0'){ //if tile exists
-			if($('.square[data-row='+target_sqr+'][data-col='+ x +'] .tile').html() == $('#'+tile_id).html()){
+			if($('.square[data-row='+y+'][data-col='+ x +'] .tile').html() == $('#'+tile_id).html()){
 				should_i_merge = true;
 			}
 		}
@@ -385,7 +430,7 @@ $(document).ready(function(){
 		console.log(this, "direction=> ", direction);
 		var add_new_tile = false, loop_move = true, loop_merge = false;
 
-		if(direction=="up" || direction=="down"){
+		// if(direction=="up" || direction=="down"){
 
 			//move
 			add_new_tile = myGame.move_tiles(direction);
@@ -402,16 +447,16 @@ $(document).ready(function(){
 
 			//if there was a merge, move again, don't merge again.
 
-		}
+		// }
 		// if(direction=="down"){
 		// 	//query each row except 4
 		// }
-		if(direction=="left"){
-			//query each col except 1
-		}
-		if(direction=="right"){
-			//query each col except 4
-		}
+		// if(direction=="left"){
+		// 	//query each col except 1
+		// }
+		// if(direction=="right"){
+		// 	//query each col except 4
+		// }
 
 		add_new_tile ? myGame.add_new_tile() : "" ;
 		// myGame.add_new_tile();
@@ -439,9 +484,9 @@ $(document).ready(function(){
 
 		var index = temp;
 
-		// this.lay_new_tile(rand_square);
+		var tile_value = this.square_values[Math.floor(Math.random() * 2)];
 		
-		var $tile = $("<div id='tile_"+Math.floor(Date.now() / 1000)*index+"' class='tile' data-x='0' data-y='0' data-merged='0'>"+this.square_values[Math.floor(Math.random() * 2)]+"</div>");
+		var $tile = $("<div id='tile_"+Math.ceil(Date.now() / 1000)*index+"' class='tile' data-x='0' data-y='0' data-merged='0' data-value='"+tile_value+"'>"+tile_value+"</div>");
 		$($squares[index]).html($tile);
 
 		$tile.attr('data-x', $tile.parent().attr('data-col'));
